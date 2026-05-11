673ff80da1b850393beea0a265b01facf91b3728
Jamesjsfo <jsfernandez@uamv.edu.ni>
2026-05-11 12:31:47 -0600

    ejercicios

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,47 @@
+﻿// temperatura de la semana
+int[] temperaturas = new int[7];
+string[] dias = new string[7] { "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo" };
+int promedio;
+
+int maxima = 0; 
+int minima = 0; 
+
+for (int i = 0; i < 7; i++)
+{
+    Console.WriteLine("Ingrese la temperatura del dia " + dias[i] + ": ");
+    temperaturas[i] = int.Parse(Console.ReadLine());
+
+    // Si es el primer día, inicializamos maxima y minima con este valor
+    if (i == 0)
+    {
+        maxima = temperaturas[i];
+        minima = temperaturas[i];
+    }
+    else
+    {
+        // Para los días siguientes, comparamos normalmente
+        if (temperaturas[i] > maxima)
+        {
+            maxima = temperaturas[i];
+        }
+
+        if (temperaturas[i] < minima)
+        {
+            minima = temperaturas[i];
+        }
+    }
+}
+
+for (int i = 0; i < 7; i++)
+{
+    Console.WriteLine("la temperatura de el dia " + dias[i] + " es: " + temperaturas[i]);
+}
+
+promedio = temperaturas.Sum() / 7;
+Console.WriteLine("El promedio de las temperaturas es: " + promedio);
+Console.ForegroundColor = ConsoleColor.Green; // Cambia el color de la consola a verde para resaltar la temperatura máxima
+Console.WriteLine("La temperatura máxima fue: " + maxima);
+Console.ResetColor(); 
+Console.ForegroundColor = ConsoleColor.Red; // Cambia el color de la consola a rojo para resaltar la temperatura mínima
+Console.WriteLine("La temperatura mínima fue: " + minima);
+Console.ResetColor(); 
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Ventas semanales de una tienda//
+
+double[] ventasSemanales = new double[7];
+string[] diasSeman = { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo" };
+double suma;
+double minimo;
+try 
+{
+for (int i = 0; i < ventasSemanales.Length; i++)
+{
+    Console.Write($"Ingrese las ventas del {diasSeman[i]}: ");
+    ventasSemanales[i] = Convert.ToDouble(Console.ReadLine());
+}
+}
+catch (FormatException)
+{
+    Console.WriteLine("Error: Por favor, ingrese un número válido para las ventas.");
+    return;
+}
+minimo = ventasSemanales[0];
+for (int i = 1; i < ventasSemanales.Length; i++)
+{
+    if (ventasSemanales[i] < minimo)
+    {
+        minimo = ventasSemanales[i];
+    }
+}
+
+for (int i = 0; i < ventasSemanales.Length; i++)
+{
+    Console.WriteLine($"Ventas del {diasSeman[i]}: {ventasSemanales[i]:C}");
+}
+suma = 0;
+for (int i = 0; i < ventasSemanales.Length; i++)
+{
+    suma += ventasSemanales[i];
+}
+Console.WriteLine($"Total de ventas semanales: {suma:C}");
+//registrar si cada dia la venta fue mayor a 150//
+for (int i = 0; i < ventasSemanales.Length; i++)
+{
+    if (ventasSemanales[i] > 150)
+    {
+        Console.WriteLine($"El {diasSeman[i]} tuvo una venta mayor a $150.");
+    }
+    else
+    {
+        Console.WriteLine($"El {diasSeman[i]} no tuvo una venta mayor a $150.");
+    }
+}
